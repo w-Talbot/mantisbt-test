68166b33fa2e28b4e54dde0769244e07013cfafa
w-talbot <wesstalbot@gmail.com>
2022-03-25 11:17:03 +0000

    First commit - Original code from MantisBT - working

---
+++ mantisbt/code/www/mantisbt/public/config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'mysql';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantisdb';
+$g_db_username            = 'root';
+$g_db_password            = 'root';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'IuhPTkuy4haovQYHELUA67hCZMRqIENx4RYMjXQu2dA=';
